89f442204358f7adf4c2f4fa3f615174ceacbbc9
JustBoyke <mail@boykevanvugt.nl>
2022-01-07 04:27:11 +0100

    Test for MYSQL movies

---
+++ data/getMovies.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php
+$con = mysqli_connect('db2.dixiehosting.nl','cineplus','xCibCzUgW9bj8BQ4','lucasnijssen_cineplus');
+if (!$con) {
+  die('Could not connect: ' . mysqli_error($con));
+}
+
+mysqli_select_db($con,"ajax_demo");
+$sql="SELECT * FROM movies";
+$result = mysqli_query($con,$sql);
+
+while($row = mysqli_fetch_array($result)) {
+  echo '<a href="/player.html?iv=' . $row['movie_id'] . '"> <img class="row__poster row_posterLarge" src="' . $row['cover'] . '" alt="" /></a>';
+}
+mysqli_close($con);
+?>
+</body>
+</html>
