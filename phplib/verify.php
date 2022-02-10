98163338474670ee5e503add1e47480e985eea37
JustBoyke <mail@boykevanvugt.nl>
2022-02-10 03:21:37 +0100

    Tring to get the code box working

--- phplib/verify.php
+++ phplib/verify.php
@@ -1,5 +1,36 @@
 <?php
 require_once "config.php";
 $gebruikersid = $_SESSION["id"];
+$code = $_POST['verifycode'];
+echo "<script>console.log('$code')";
 
 ?>
+
+<?php 
+include_once "config.php";
+$gebruikersid = $_SESSION["id"];
+
+$conn = new mysqli($db_servername, $db_username, $db_password, $db_dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select * from users WHERE id = '$gebruikersid'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $verstat = $row["verify"];
+    }
+} else {
+
+}
+$conn->close();
+
+if($verstat == 1){
+    header("location: index.html");
+}else{
+    
+}
