852e5505107fbf2dc9a386e48d33a95c7981df71
JustBoyke <mail@boykevanvugt.nl>
2022-02-10 02:39:13 +0100

    Fixed typo

--- phplib/datacheck.php
+++ phplib/datacheck.php
@@ -1,11 +1,12 @@
 <?php ##Check the status of the user
+require_once "config.php";
     $gebruikersid = $_SESSION["id"];
     $sql = "select * from users WHERE id = '$gebruikersid'";
     $result = $link->query($sql);
     if ($result->num_rows > 0) { while($row = $result->fetch_assoc()) { $verstat = $row["verify"]; } } else { }
     $conn->close();
 
-    if($verify == 1){
+    if($verstat == 1){
         header("location: verify.html");
     }else{
 
