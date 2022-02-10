919e8ee50660a53fc2b579007351277ff1c68f98
JustBoyke <mail@boykevanvugt.nl>
2022-02-10 02:42:49 +0100

    Fixed config issue

--- phplib/datacheck.php
+++ phplib/datacheck.php
@@ -1,3 +1,23 @@
 <?php 
+include_once "./config.php";
+$gebruikersid = $_SESSION["id"];
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select * from users WHERE id = '$gebruikersid'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $verstat = $row["verify"];
+    }
+} else {
+
+}
+$conn->close();
 ?>
