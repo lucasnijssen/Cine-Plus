40c85c526dd1593556ab9a40b0561a8e722f2460
JustBoyke <mail@boykevanvugt.nl>
2022-02-10 02:40:50 +0100

    test ing

--- phplib/datacheck.php
+++ phplib/datacheck.php
@@ -1,14 +1,3 @@
-<?php ##Check the status of the user
-require_once "config.php";
-    $gebruikersid = $_SESSION["id"];
-    $sql = "select * from users WHERE id = '$gebruikersid'";
-    $result = $link->query($sql);
-    if ($result->num_rows > 0) { while($row = $result->fetch_assoc()) { $verstat = $row["verify"]; } } else { }
-    $conn->close();
+<?php 
 
-    if($verstat == 1){
-        header("location: verify.html");
-    }else{
-
-    }
 ?>
