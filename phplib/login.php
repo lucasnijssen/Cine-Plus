4c2055a2c994db7b195c68a36caf28354c5a4c4d
JustBoyke <mail@boykevanvugt.nl>
2022-02-10 03:02:25 +0100

    Fixed sql statement issue

--- phplib/login.php
+++ phplib/login.php
@@ -71,10 +71,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             if ($conn->connect_error) {
                                 die("Connection failed: " . $conn->connect_error);
                             }
-                            $create_session = 'INSERT INTO `sessions` (`userid`, `sessionid`, `ip`, `device`) VALUES ($_SESSION["username"],$_SESSION["id"],UNKNOWN,UNKNOWN2)';
+                            $ses = $_SESSION["username"];
+                            $ses_id = $_SESSION["id"];
+                            $create_session = "INSERT INTO `sessions` (`userid`, `sessionid`, `ip`, `device`) VALUES ('$ses','$ses_id','UNKNOWN','UNKNOWN2')";
 
                             if ($conn->query($create_session) === TRUE) {
-                                echo '<meta http-equiv="refresh" content="0; url=#" />';
+                                
                             } else {
                                 echo "Error: " . $create_session . "<br>" . $conn->error;
                             }
