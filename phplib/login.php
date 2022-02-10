86a539c961a6383f502d2a92fe2084bfec8911b3
JustBoyke <mail@boykevanvugt.nl>
2022-02-10 16:38:41 +0100

    ifset statements

--- phplib/login.php
+++ phplib/login.php
@@ -2,8 +2,6 @@
  ini_set('session.gc_maxlifetime', 864000);
  session_set_cookie_params(864000);
 session_start();
-ob_start();
-setcookie('login_error', '', time() + (86400 * 30), "/"); // 86400 = 1 day
 
 
 echo '<script>console.log("' . $error_mes . '")</script>';
@@ -48,6 +46,15 @@ $username_err = $password_err = "";
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    if(!issset($_POST["username"])){
+        echo '<script>swal.fire("Gebruikersnaam", "Je hebt geen gebruikersnaam opgegeven", "error");</script>';
+        die();
+    }
+    if(!issset($_POST["password"])){
+        echo '<script>swal.fire("Wachtwoord", "Je hebt geen wachtwoord opgegeven", "error");</script>';
+        die();
+    }
 
 
     if(empty(trim($_POST["username"]))){
