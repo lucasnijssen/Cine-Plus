0dff0abb3bb4f12483b033f09669b120e8d437d3
JustBoyke <mail@boykevanvugt.nl>
2022-02-10 03:52:10 +0100

    Logout if session is destroyed on the server

--- phplib/login_check.php
+++ phplib/login_check.php
@@ -6,3 +6,33 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 ?>
+
+
+<?php 
+$gebruikersid = $_SESSION["id"];
+$phsid = $_COOKIE["PHPSESSID"];
+$conn = new mysqli($db_servername, $db_username, $db_password, $db_dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select * from users WHERE id = '$gebruikersid'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $user_sessionid = $row["sesid"];
+    }
+} else {
+
+}
+$conn->close();
+
+if($user_sessionid == $phsid){
+    
+}else{
+    header("location: logout.html");
+}
+?>
