0dbc9825a9d9be5d003f0284d0dcd26a26c7e371
JustBoyke <mail@boykevanvugt.nl>
2022-02-11 04:48:07 +0100

    Moved security check

--- admin/nav.php
+++ admin/nav.php
@@ -1,3 +1,6 @@
+<?php
+include_once("checkrank.php");
+?>
 <ul class="navbar-nav text-light" id="accordionSidebar">
     <li class="nav-item"><a class="nav-link" href="index.html"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
     <li class="nav-item"><a class="nav-link" href="movies.html"><i class="fas fa-table"></i><span>films</span></a></li>
