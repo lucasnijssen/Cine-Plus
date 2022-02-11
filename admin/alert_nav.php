d24ac175e5cf821ac21626095b5c9b004b42849e
JustBoyke <mail@boykevanvugt.nl>
2022-02-11 03:46:38 +0100

    alert center test

--- admin/alert_nav.php
+++ admin/alert_nav.php
@@ -3,28 +3,36 @@
 <li class="nav-item dropdown no-arrow mx-1">
     <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="badge bg-danger badge-counter">3+</span><i class="fas fa-bell fa-fw"></i></a>
         <div class="dropdown-menu dropdown-menu-end dropdown-list animated--grow-in">
-            <h6 class="dropdown-header">alerts center</h6><a class="dropdown-item d-flex align-items-center" href="#">
-                <div class="me-3">
-                    <div class="bg-primary icon-circle"><i class="fas fa-file-alt text-white"></i></div>
-                </div>
-                <div><span class="small text-gray-500">December 12, 2019</span>
-                    <p>A new monthly report is ready to download!</p>
-                </div>
-            </a><a class="dropdown-item d-flex align-items-center" href="#">
-                <div class="me-3">
-                    <div class="bg-success icon-circle"><i class="fas fa-donate text-white"></i></div>
-                </div>
-                <div><span class="small text-gray-500">December 7, 2019</span>
-                    <p>$290.29 has been deposited into your account!</p>
-                </div>
-            </a><a class="dropdown-item d-flex align-items-center" href="#">
-                <div class="me-3">
-                    <div class="bg-warning icon-circle"><i class="fas fa-exclamation-triangle text-white"></i></div>
-                </div>
-                <div><span class="small text-gray-500">December 2, 2019</span>
-                    <p>Spending Alert: We've noticed unusually high spending for your account.</p>
-                </div>
-            </a><a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+            <h6 class="dropdown-header">alerts center</h6>
+                <?php 
+                    include_once("../phplib/config.php");
+                    $conn = new mysqli($db_servername, $db_username, $db_password, $db_dbname);
+                    // Check connection
+                    if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                    }
+
+                    $sql = "select * from movies";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo '<a class="dropdown-item d-flex align-items-center" href="#">';
+                        echo '<div class="me-3">';
+                        echo '<div class="bg-primary icon-circle"><i class="fas fa-file-alt text-white"></i></div>';
+                        echo '</div>';
+                        echo '<div><span class="small text-gray-500">December 12, 2019</span>';
+                        echo '<p>A new monthly report is ready to download!</p>';
+                        echo '</div></a>';
+                        }
+                    } else {
+                        echo '<div><span class="small text-gray-500">-</span>';
+                        echo '<p>Geen nieuwe meldingen</p>';
+                        echo '</div></a>';
+                    }
+                    $conn->close();
+                ?>
         </div>
     </div>
 </li>
