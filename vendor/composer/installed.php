bc0742a4e767a4fad5881d36cfc8969e8aa8e360
JustBoyke <mail@boykevanvugt.nl>
2022-02-11 19:54:37 +0100

    Added stripe composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '022d19bce5d6266e334d5b4f3c324d76b38ca9e0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '022d19bce5d6266e334d5b4f3c324d76b38ca9e0',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v7.113.0',
+            'version' => '7.113.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => '1aef1ccffad48f39952073e0ed53cb8f3f1b1d8c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
