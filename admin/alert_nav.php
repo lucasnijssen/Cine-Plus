cf13b6560753c1caa1e01349a41f8294ed43e2d0
JustBoyke <mail@boykevanvugt.nl>
2022-02-11 03:53:04 +0100

    Updated message center

--- admin/alert_nav.php
+++ admin/alert_nav.php
@@ -7,8 +7,13 @@
     }
 
     $sql = "select * from movies";
+    $sql2 = "select * from movies";
+
     $result = $conn->query($sql);
+    $result2 = $conn->query($sql2);
+
     $alert_count = $result->num_rows;
+    $message_count = $result2->num_rows;
 ?>
 
 
@@ -19,8 +24,6 @@
         <div class="dropdown-menu dropdown-menu-end dropdown-list animated--grow-in">
             <h6 class="dropdown-header">alerts center</h6>
                 <?php 
-               
-
                     if ($result->num_rows > 0) {
                     // output data of each row
                     while($row = $result->fetch_assoc()) {
@@ -43,41 +46,30 @@
     </div>
 </li>
 <li class="nav-item dropdown no-arrow mx-1">
-    <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="badge bg-danger badge-counter">7</span><i class="fas fa-envelope fa-fw"></i></a>
+    <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="badge bg-danger badge-counter"><?php echo $message_count; ?></span><i class="fas fa-envelope fa-fw"></i></a>
         <div class="dropdown-menu dropdown-menu-end dropdown-list animated--grow-in">
-            <h6 class="dropdown-header">alerts center</h6><a class="dropdown-item d-flex align-items-center" href="#">
-                <div class="dropdown-list-image me-3"><img class="rounded-circle" src="assets/img/avatars/avatar4.jpeg">
-                    <div class="bg-success status-indicator"></div>
-                </div>
-                <div class="fw-bold">
-                    <div class="text-truncate"><span>Hi there! I am wondering if you can help me with a problem I've been having.</span></div>
-                    <p class="small text-gray-500 mb-0">Emily Fowler - 58m</p>
-                </div>
-            </a><a class="dropdown-item d-flex align-items-center" href="#">
-                <div class="dropdown-list-image me-3"><img class="rounded-circle" src="assets/img/avatars/avatar2.jpeg">
-                    <div class="status-indicator"></div>
-                </div>
-                <div class="fw-bold">
-                    <div class="text-truncate"><span>I have the photos that you ordered last month!</span></div>
-                    <p class="small text-gray-500 mb-0">Jae Chun - 1d</p>
-                </div>
-            </a><a class="dropdown-item d-flex align-items-center" href="#">
-                <div class="dropdown-list-image me-3"><img class="rounded-circle" src="assets/img/avatars/avatar3.jpeg">
-                    <div class="bg-warning status-indicator"></div>
-                </div>
-                <div class="fw-bold">
-                    <div class="text-truncate"><span>Last month's report looks great, I am very happy with the progress so far, keep up the good work!</span></div>
-                    <p class="small text-gray-500 mb-0">Morgan Alvarez - 2d</p>
-                </div>
-            </a><a class="dropdown-item d-flex align-items-center" href="#">
-                <div class="dropdown-list-image me-3"><img class="rounded-circle" src="assets/img/avatars/avatar5.jpeg">
-                    <div class="bg-success status-indicator"></div>
-                </div>
-                <div class="fw-bold">
-                    <div class="text-truncate"><span>Am I a good boy? The reason I ask is because someone told me that people say this to all dogs, even if they aren't good...</span></div>
-                    <p class="small text-gray-500 mb-0">Chicken the Dog · 2w</p>
-                </div>
-            </a><a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+            <h6 class="dropdown-header">alerts center</h6>
+            <a class="dropdown-item d-flex align-items-center" href="#">
+                <?php 
+                    if ($result2->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result2->fetch_assoc()) {
+                        echo '<a class="dropdown-item d-flex align-items-center" href="#">';
+                        echo '<div class="dropdown-list-image me-3"><img class="rounded-circle" src="assets/img/avatars/avatar4.jpeg">';
+                        echo '<div class="bg-success status-indicator"></div>';
+                        echo '</div>';
+                        echo '<div class="fw-bold">>';
+                        echo '<div class="text-truncate"><span>Hi there! I am wondering if you can help me with a problem Ive been having.</span></div>';
+                        echo '<p class="small text-gray-500 mb-0">Emily Fowler</p>'
+                        echo '</div></a>';
+                        }
+                    } else {
+                        echo '<div><span class="small text-gray-500">-</span>';
+                        echo '<p>Geen nieuwe meldingen</p>';
+                        echo '</div></a>';
+                    }
+                    $conn->close();
+                ?>
         </div>
     </div>
     <div class="shadow dropdown-list dropdown-menu dropdown-menu-end" aria-labelledby="alertsDropdown"></div>
