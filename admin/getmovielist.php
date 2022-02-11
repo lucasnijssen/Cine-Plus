3f69e73077a93cdbd54a98ed4852899f56a8d1e8
JustBoyke <mail@boykevanvugt.nl>
2022-02-11 04:16:28 +0100

    Deleting test

--- admin/getmovielist.php
+++ admin/getmovielist.php
@@ -17,7 +17,8 @@ if ($result->num_rows > 0) {
         echo '<td>' . $row["title"] . '</td>';
         echo '<td>' . $row["info"] . '</td>';
         echo '<td>' . $row["cdn"] . '</td>';
-        echo "<td><a href='./show.php?id=" . $row["id"]. "' class='btn btn-success' role='button' style='margin: inherit;'>Info</a></td>";
+        echo "<td><a href='./show.php?id=" . $row["id"]. "' class='btn btn-success' role='button' style='margin: inherit;'>Info</a>";
+        echo "<button type='button' class='btn btn-danger' data-toggle='modal' data-product-id='" . $row["id"]. "' data-target='#verwijderProduct' style='margin: inherit;'>Verwijder</button></td>"
         echo '</tr>';
     }
 } else {
@@ -32,3 +33,59 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
+
+<?php
+if (isset($_POST["confirm_delete"])) {
+$deleteprod_id = $_POST["product_id"];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "DELETE FROM `movies` WHERE `id` = $deleteprod_id";
+$result = $conn->query($sql);
+if ($conn->query($sql) === TRUE) {
+	echo '<meta http-equiv="refresh" content="0; url=#" />';
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+}else{
+
+}
+?>
+
+<div class="modal fade" id="verwijderProduct" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Verwijder Film</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+		<form method="post" target="_self" style="width: 100%;height: auto;">
+<p>Vink aan om verwijdering te bevestigen</p>
+<input class="form-control" type="checkbox" name="confirm_delete" style="height:24px;" required="" value="">
+<input class="form-control" type="hidden" id="product_id" name="product_id" value="">
+
+
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-success" type="submit">Verwijder Film</button>
+        </form>
+        <button type="button" class="btn btn-warning" data-dismiss="modal">Annuleer</button>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+$('#verwijderProduct').on('show.bs.modal', function(e) {
+    var bookId = $(e.relatedTarget).data('product-id');
+    $(e.currentTarget).find('input[name="product_id"]').val(bookId);
+});
+</script>
