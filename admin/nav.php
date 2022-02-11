2c475a437ee97735e61716d9eee728b90bc885d6
JustBoyke <mail@boykevanvugt.nl>
2022-02-11 03:57:03 +0100

    Moved nav to central place

---
+++ admin/nav.php
@@ -0,0 +1,4 @@
+<ul class="navbar-nav text-light" id="accordionSidebar">
+    <li class="nav-item"><a class="nav-link" href="index.html"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
+    <li class="nav-item"><a class="nav-link" href="movies.html"><i class="fas fa-table"></i><span>films</span></a></li>
+</ul>
