d858b8d84db238906951a740982a262b116b4c9d
JustBoyke <mail@boykevanvugt.nl>
2022-02-12 01:09:23 +0100

    User overview from admin panel

--- admin/nav.php
+++ admin/nav.php
@@ -3,5 +3,6 @@ include_once("checkrank.php");
 ?>
 <ul class="navbar-nav text-light" id="accordionSidebar">
     <li class="nav-item"><a class="nav-link" href="index.html"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
-    <li class="nav-item"><a class="nav-link" href="movies.html"><i class="fas fa-table"></i><span>films</span></a></li>
+    <li class="nav-item"><a class="nav-link" href="movies.html"><i class="fas fa-table"></i><span>Films</span></a></li>
+    <li class="nav-item"><a class="nav-link" href="users.html"><i class="fas fa-table"></i><span>Gebruikers</span></a></li>
 </ul>
