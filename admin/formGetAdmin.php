f95afefe6cbe86c352fb00904f64261cb34ab13c
JustBoyke <mail@boykevanvugt.nl>
2022-02-12 03:36:29 +0100

    Prepared send message function (testing)

---
+++ admin/formGetAdmin.php
@@ -0,0 +1,21 @@
+<?php 
+include_once("../phplib/config.php");
+$conn = new mysqli($db_servername, $db_username, $db_password, $db_dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM `users` WHERE `ranglevel` >= 10";
+
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo '<option value="' . $row["id"] . '">'. $row["username"] . '</option>';
+    }
+} else {
+
+}
+$conn->close();
+?>
