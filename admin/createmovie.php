db61d17952dfe53c11f54cc2dca1272ac8f57d87
JustBoyke <mail@boykevanvugt.nl>
2022-02-12 19:39:34 +0100

    Set namespaced  and linked

--- admin/createmovie.php
+++ admin/createmovie.php
@@ -9,7 +9,7 @@ include_once("../phplib/config.php");
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Bericht sturen - Cine-Plus Admin</title>
+    <title>Film Aanmaken - Cine-Plus Admin</title>
     <meta name="description" content="Administratie pagina van Cine-Plus">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
@@ -51,10 +51,10 @@ include_once("../phplib/config.php");
                     </div>
                 </nav>
                 <div class="container-fluid">
-                    <h3 class="text-dark mb-4">Bericht Versturen</h3>
+                    <h3 class="text-dark mb-4">Film Aanmaken</h3>
                     <div class="card shadow">
                     <div class="card-header d-flex justify-content-between align-items-center">
-                            <h6 class="text-primary fw-bold m-0">Bericht</h6>
+                            <h6 class="text-primary fw-bold m-0">Cre&euml;r Film</h6>
                         </div>
                         <div class="card-body">
                             <form action="#" method="post">
