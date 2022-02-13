dc9f19d28ca0cfac59cdd9e4a9a610c2e892f0c4
JustBoyke <mail@boykevanvugt.nl>
2022-02-13 20:18:03 +0100

    See who sends the message

--- admin/infogetters/sended.php
+++ admin/infogetters/sended.php
@@ -14,6 +14,25 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<tr>";
         echo '<td>' . $row["message_short"] . '</td>';
+
+        $senderid = $row["user"];
+        $conn = new mysqli($db_servername, $db_username, $db_password, $db_dbname);
+        if($senderid == "s"){
+            $sendername = "Systeem";
+            $conn->close();
+        }else{
+            $sql3 = "select * from users WHERE id=$senderid";
+            $result3 = $conn->query($sql3);
+            if ($result3->num_rows > 0) {
+                while($row = $result3->fetch_assoc()) {
+                    $sendername = $row["username"];
+                    }
+                } else {
+                    $sendername = "Onbekend";
+                }
+                $conn->close();
+        }
+        echo '<td>' . $sendername . '</td>';
         echo "<td><a href='javascript:void(0);'  onclick='showNumer" . $row['id'] . "();' class='btn btn-success' role='button' style='margin: inherit;'>Bekijk</a>";
         echo "<a href='?delmov=" . $row["id"]. "' class='btn btn-danger' role='button' style='margin: inherit;'>Delete</a></td>";
         echo "<script>function showNumer" . $row['id'] . "() { Swal.fire({title: '" . $row["message_short"] . "',text:'" . $row["message"] . "', showConfirmButton: true }) }</script>";
