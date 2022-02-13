c4538b62faaefacf5ae1a100f9dc3da59ecbb640
JustBoyke <mail@boykevanvugt.nl>
2022-02-13 19:04:33 +0100

    Added custom CSS

--- admin/createAdminAlert.php
+++ admin/createAdminAlert.php
@@ -12,6 +12,7 @@ include_once("../phplib/config.php");
     <title>Admin Alert sturen - Cine-Plus Admin</title>
     <meta name="description" content="Administratie pagina van Cine-Plus">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/custom.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
