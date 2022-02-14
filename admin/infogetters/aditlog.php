63274f888f7398a8f341e1a49ce99cc1701764ea
JustBoyke <mail@boykevanvugt.nl>
2022-02-14 21:40:14 +0100

    Audit log fix?

--- admin/infogetters/aditlog.php
+++ admin/infogetters/aditlog.php
@@ -1,14 +1,14 @@
 <?php
 include_once("../../phplib/config.php");
 // Create connection
-$conn = new mysqli($db_servername, $db_username, $db_password, $db_dbname);
+$audit_conn = new mysqli($db_servername, $db_username, $db_password, $db_dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($audit_conn->connect_error) {
+    die("Connection failed: " . $audit_conn->connect_error);
 }
 
-$sql = "INSERT INTO `audit_log`(`gebruiker`, `actie`, `info`) VALUES ('$audit_user','$audit_actie','$audit_info')";
-if ($conn->query($sql) === TRUE) {
+$audit_sql = "INSERT INTO `audit_log` (`gebruiker`, `actie`, `info`) VALUES ('$audit_user','$audit_actie','$audit_info')";
+if ($audit_conn->query($audit_sql) === TRUE) {
 
 } else {
     
