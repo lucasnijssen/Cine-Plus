541a9f7e9ef3d4a4c530bceb171b8cc9f34c704d
JustBoyke <mail@boykevanvugt.nl>
2022-02-14 04:56:33 +0100

    Build my own online tracker

--- data/trackers.php
+++
@@ -1,9 +0,0 @@
-<!-- Begin Web-Stat code v 7.0 -->
-<span id="wts2091536"></span>
-<script>var wts=document.createElement('script');wts.async=true;
-wts.src='https://app.ardalio.com/log7.js';document.head.appendChild(wts);
-wts.onload = function(){ wtslog7(2091536,2); };
-</script><noscript><a href="https://www.web-stat.com">
-<img src="https://app.ardalio.com/7/2/2091536.png" 
-alt="Web-Stat traffic analytics"></a></noscript>
-<!-- End Web-Stat code v 7.0 -->
