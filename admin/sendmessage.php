20a40ce4c3d66fbb61ccf7f6bb6f58d0268aab0a
JustBoyke <mail@boykevanvugt.nl>
2022-02-14 21:44:56 +0100

    Error logging

--- admin/sendmessage.php
+++ admin/sendmessage.php
@@ -162,7 +162,7 @@ if ($conn->query($sql) === TRUE) {
     $audit_conn = new mysqli($db_servername, $db_username, $db_password, $db_dbname);
     if ($audit_conn->connect_error) { die("Connection failed: " . $audit_conn->connect_error); }
     $audit_sql = "INSERT INTO `audit_log` (`gebruiker`, `actie`, `info`) VALUES ('$audit_user','$audit_actie','$audit_info')";
-    if ($audit_conn->query($audit_sql) === TRUE) { $audit_conn->close(); } else { $audit_conn->close(); }
+    if ($audit_conn->query($audit_sql) === TRUE) { $audit_conn->close(); } else { echo "Error: " . $sql . "<br>" . $conn->error; $audit_conn->close(); }
     //Audit log end
 
     error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
