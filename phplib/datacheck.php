51e96ce8c6955859d4152e877274c036c78334b9
JustBoyke <mail@boykevanvugt.nl>
2022-02-20 04:41:11 +0100

    Moved include

--- phplib/datacheck.php
+++ phplib/datacheck.php
@@ -1,6 +1,5 @@
 <?php 
 include_once "config.php";
-include_once "../paysystem/checkSubStatus.php";
 $gebruikersid = $_SESSION["id"];
 
 $conn = new mysqli($db_servername, $db_username, $db_password, $db_dbname);
