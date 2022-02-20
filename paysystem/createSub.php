17b779c51e410a7445045cbaac57547a56dfc1e5
JustBoyke <mail@boykevanvugt.nl>
2022-02-21 00:21:40 +0100

    Go to payment page

--- paysystem/createSub.php
+++ paysystem/createSub.php
@@ -23,7 +23,7 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $url = "https://api.stripe.com/v1/subscriptions";
+    $url = "https://api.stripe.com/v1/checkout/sessions";
 
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_URL, $url);
@@ -31,32 +31,30 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     
     $headers = array(
-       "Authorization: Bearer $strkey",
-       "Content-Type: application/x-www-form-urlencoded",
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    
-    $data = "customer=$dta_usr_stripeid&items[0][price]=price_1KV5sIC4DanyFX8In3lN2wb8&trial_period_days=14";
-    
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-    
-    //for debug only!
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    
-    $resp = curl_exec($curl);
-    curl_close($curl);
+        "Authorization: Bearer $strkey",
+        "Content-Type: application/x-www-form-urlencoded",
+     );
+     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+     
+     $data = "customer=$dta_usr_stripeid&line_items[][price]=price_1KV5sIC4DanyFX8In3lN2wb8&line_items[][quantity]=1&subscription_data[trial_period_days]=14&mode=subscription&success_url=https://dev.cine-plus.nl?session_id={CHECKOUT_SESSION_ID}&cancel_url=https://dev.cine-plus.nl";
+     
+     curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+     
+     //for debug only!
+     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+     
+     $resp = curl_exec($curl);
+     curl_close($curl);
     $json = json_encode($resp);
     $obj = json_decode($resp);
-    $checkStripeStat = $obj->{'status'};
-        if($checkStripeStat == "trialing" || $checkStripeStat == "active"){
-            $conn = new mysqli($db_servername, $db_username, $db_password, $db_dbname);
-            if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
-            $sql = "UPDATE `users` SET `sub`=1 WHERE `username` = '$dta_usr_mail'";
-            if ($conn->query($sql) === TRUE) { } else { echo "Error: " . $sql . "<br>" . $conn->error; }
-            $conn->close();
-            header("location: index.html");
-        }
+    $geturl = $obj->{'url'};
+    header("location: $geturl");
 }
+
+?>
+
+<?php
+
 
 ?>
