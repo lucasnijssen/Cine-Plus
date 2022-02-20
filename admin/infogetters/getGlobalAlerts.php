e1a8e476f2434c7fe7082ea6052d9e7747f64ab6
JustBoyke <mail@boykevanvugt.nl>
2022-02-20 02:33:47 +0100

    Resend Function

--- admin/infogetters/getGlobalAlerts.php
+++ admin/infogetters/getGlobalAlerts.php
@@ -15,10 +15,10 @@ if ($result->num_rows > 0) {
         echo "<tr>";
         echo '<td>' . $row["mes_title"] . '</td>';
         echo '<td>' . $row["mes_text"] . '</td>';
-        echo '<td><i style="margin-right:0rem;" class="fas fa-' . $row["type"] . '"></i></td>';
         echo "<td><a href='javascript:void(0);'  onclick='showNumer" . $row['id'] . "();' class='btn btn-success' role='button' style='margin: inherit;'>Bekijk</a>";
-        echo "</td>";
+        echo "<a href='javascript:void(0);'  onclick='resendNumer" . $row['id'] . "();' class='btn btn-danger' role='button' style='margin: inherit;'>Resend</a></td>";
         echo "<script>function showNumer" . $row['id'] . "() { Swal.fire({title: '" . $row["mes_title"] . "',text:'" . $row["mes_text"] . "', showConfirmButton: true }) }</script>";
+        echo "<script>function resendNumer" . $row['id'] . "() { ws.send(JSON.stringify({ id: 'send-admin-alert', titel: '" . $row["mes_title"] . "', text: '" . $row["mes_text"] . "' })); ws.send(JSON.stringify({ id: 'audit-log', user: " . $gebid . ", actie: 'Admin Systeem Notifactie verstuurd', info: 'Verstuurde notificatie met titel " . $row["mes_title"] . "' }));}</script>";
         echo '</tr>';
     }
 } else {
