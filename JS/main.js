28ebbd9006a74f31f41ae52850a2d62c9dd9f055
Lucasnijssen <nijssenlucas@gmail.com>
2022-02-22 09:01:38 +0100

    PWA v1 (test)

--- JS/main.js
+++ JS/main.js
@@ -51,3 +51,18 @@ document.addEventListener('keydown', (event) => {
       }
 		}
   });
+
+  window.addEventListener('load', () => {
+    registerSW
+  });
+ 
+  async function registerSW() {
+    if ('serviceWorker' in navigator) {
+      try{
+        await navigator.serviceWorker.register('./sw.js');
+      } catch (e) {
+        console.log('SW registration failed');
+      }
+    }
+  }
+
